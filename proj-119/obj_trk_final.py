53e705a40b5010954717a073089c89adfd7912c0
avani131313 <97681549+avani131313@users.noreply.github.com>
2023-06-18 12:38:39 +0400

    Update obj_trk_final.py

--- proj-119/obj_trk_final.py
+++ proj-119/obj_trk_final.py
@@ -43,7 +43,7 @@ def goal_track(img,bbox):
 def drawBox(img,bbox):
     #write code for this function here
     x,y,w,h = int(bbox[0],int(bbox[1]),int(bbox[3])
-    cv2.rectangle (img,(x,y),((x+w),(y+h)),(255,0,255)3,1)
+    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
     cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
     pass
 
